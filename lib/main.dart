6a2f963233454a4a973157aac98c0857f2132634
LbnMahs <labanmahs2002@gmail.com>
2024-02-10 16:53:58 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.greenAccent,
+    brightness: Brightness.dark
+  ),
+  // textTheme: GoogleFonts.montserratTextTheme()
+);
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meals App',
+      theme: theme,
+      home: const Center()
+    );
+  }
+}
