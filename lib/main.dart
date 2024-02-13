d686acc7aa2fd9f863e9f7d4cf3a8c2e009871fa
LbnMahs <labanmahs2002@gmail.com>
2024-02-13 18:56:03 +0300

    file refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hoopshub/presentation/screens/home_tab.dart';
+import 'package:hoopshub/views/screens/home_tab.dart';
 
 void main() {
   runApp(const MyApp());
