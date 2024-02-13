e19936028d2122fc0eb6ad40a8579334fba0a2d8
LbnMahs <labanmahs2002@gmail.com>
2024-02-14 02:57:52 +0300

    team bloc getTeams logic

--- lib/bloc/teams_bloc.dart
+++ lib/bloc/teams_bloc.dart
@@ -1,13 +1,28 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/foundation.dart';
 
+import 'package:hoopshub/data/repository/team_repository.dart';
+import '../models/team_model.dart';
+
 part 'teams_event.dart';
 part 'teams_state.dart';
 
 class TeamsBloc extends Bloc<TeamsEvent, TeamsState> {
-  TeamsBloc() : super(TeamsInitial()) {
-    on<TeamsEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final TeamRepository teamsRepository;
+
+  TeamsBloc(this.teamsRepository) : super(TeamsInitial()) {
+    on<TeamsFetched>(_getTeams);
+  }
+
+  void _getTeams(TeamsFetched event, Emitter<TeamsState> emit) async {
+    emit(TeamsLoading());
+    try {
+      final easternTeams = await teamsRepository.getTeamsByConference('East');
+      final westernTeams = await teamsRepository.getTeamsByConference('West');
+
+      emit(TeamsSuccess(easternTeams, westernTeams));
+    } catch (e) {
+      emit(TeamsFailure(e.toString()));
+    }
   }
 }
--- lib/bloc/teams_state.dart
+++ lib/bloc/teams_state.dart
@@ -7,6 +7,15 @@ final class TeamsInitial extends TeamsState {}
 
 final class TeamsLoading extends TeamsState {}
 
-final class TeamsSuccess extends TeamsState {}
+final class TeamsSuccess extends TeamsState {
+  final List<Team> easternTeams;
+  final List<Team> westernTeams;
 
-final class TeamsFailure extends TeamsState {}
+  TeamsSuccess(this.easternTeams, this.westernTeams);
+}
+
+final class TeamsFailure extends TeamsState {
+  final String error;
+
+  TeamsFailure(this.error);
+}
