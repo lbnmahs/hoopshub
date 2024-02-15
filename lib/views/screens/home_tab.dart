9f58a13a72f158c3a4162cde2773cfe86ce18f94
LbnMahs <labanmahs2002@gmail.com>
2024-02-15 17:19:05 +0300

    Displaying the teams from the API

--- lib/views/screens/home_tab.dart
+++ lib/views/screens/home_tab.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hoopshub/bloc/teams_bloc.dart';
+import 'package:hoopshub/views/screens/teams_screen.dart';
 
 class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
@@ -11,20 +14,43 @@ class HomeTab extends StatefulWidget {
 class _HomeTabState extends State<HomeTab> {
   int _currentIndex = 0;
 
+  @override
+  void initState() {
+    super.initState();
+    context.read<TeamsBloc>().add(TeamsFetched());
+  }
+  
   void _selectTab(int index) {
     setState(() { _currentIndex = index; });
   }
   
   @override
   Widget build(BuildContext context) {
-    if(_currentIndex == 1) {}
-    
     return Scaffold(
       appBar: AppBar(
         title: const Text('HoopsHub'),
         forceMaterialTransparency: true,
       ),
-      body: const Center(),
+      body: BlocBuilder<TeamsBloc, TeamsState>(
+        builder: ((context, state) {
+          // Make sure to define TeamFailure before TeamsSuccess
+          if(state is TeamsFailure) {
+            return Center(child: Text(state.error));
+          }
+          if(state is! TeamsSuccess) {
+            return const Center(child: CircularProgressIndicator.adaptive());
+          }
+
+          final easternTeams = state.easternTeams;
+          final westernTeams = state.westernTeams;
+
+          if (_currentIndex == 1) {
+            return TeamsScreen(teams: easternTeams);
+          }
+
+          return TeamsScreen(teams: westernTeams);
+        })
+      ),
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectTab,
         items: const [
